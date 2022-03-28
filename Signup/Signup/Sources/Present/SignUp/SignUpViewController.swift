298ce1cc28e0cdcef6ebf90d7a13f9bc22f56b9e
shingha <shingha1124@gmail.com>
2022-03-28 20:10:21 +0900

    [feature] 1차 회원가입 화면 UI구성

--- Signup/Signup/Sources/Present/SignUp/SignUpViewController.swift
+++ Signup/Signup/Sources/Present/SignUp/SignUpViewController.swift
@@ -9,11 +9,70 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    let signUpTitle: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "회원가입"
+        label.textColor = .systemGreen
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 40, weight: .bold)
+        return label
+    }()
+    
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        return stackView
+    }()
+    
+    let userId: InputView = {
+        let inputView = InputView()
+        inputView.translatesAutoresizingMaskIntoConstraints = false
+        inputView.title.text = "아이디"
+        inputView.textField.placeholder = "  영문 소문자, 숫자, 특수기호(_,-), 5~20자"
+        inputView.errorLabel.text = "이미 사용중인 아이디입니다."
+        return inputView
+    }()
+    
+    let password: InputView = {
+        let inputView = InputView()
+        inputView.translatesAutoresizingMaskIntoConstraints = false
+        inputView.title.text = "비밀번호"
+        inputView.textField.placeholder = "  영문 대/소문자, 숫자, 특수문자(!@#$%) 8~16자"
+        inputView.errorLabel.text = "이미 사용중인 아이디입니다."
+        return inputView
+    }()
+    
+    let passwordCheck: InputView = {
+        let inputView = InputView()
+        inputView.translatesAutoresizingMaskIntoConstraints = false
+        inputView.title.text = "비밀번호 재확인"
+        inputView.textField.placeholder = ""
+        inputView.errorLabel.text = "이미 사용중인 아이디입니다."
+        return inputView
+    }()
+    
+    let userName: InputView = {
+        let inputView = InputView()
+        inputView.translatesAutoresizingMaskIntoConstraints = false
+        inputView.title.text = "이름"
+        inputView.textField.placeholder = ""
+        inputView.errorLabel.text = "이미 사용중인 아이디입니다."
+        return inputView
+    }()
+    
+    var inputViews: [InputView] {
+        [self.userId, self.password, self.passwordCheck, self.userName]
+    }
+    
     let model = SignUpModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         bind()
+        attribute()
         layout()
     }
     
@@ -23,17 +82,44 @@ class SignUpViewController: UIViewController {
 //        model.action.userIdEntered.send("idididid")
 //        model.action.passwordEntered.send("password")
         
-        model.action.nextButtonTapped.send()
-        
-        model.action.userIdEntered.send("****")
-        model.action.passwordEntered.send("password")
-        model.action.userIdEntered.send("idididid1234")
+//        model.action.nextButtonTapped.send()
+//
+//        model.action.userIdEntered.send("****")
+//        model.action.passwordEntered.send("password")
+//        model.action.userIdEntered.send("idididid1234")
 //        model.action.userIdEntered.send("idididid123")
 //        model.action.passwordEntered.send("password113")
     }
     
+    private func attribute() {
+        self.view.backgroundColor = .systemGray6
+    }
+    
     private func layout() {
+        let safeArea = self.view.safeAreaLayoutGuide
+        self.view.addSubview(signUpTitle)
+        signUpTitle.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        signUpTitle.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
+        signUpTitle.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor).isActive = true
+        signUpTitle.heightAnchor.constraint(equalToConstant: 80).isActive = true
         
+        self.view.addSubview(stackView)
+        stackView.topAnchor.constraint(equalTo: signUpTitle.bottomAnchor, constant: 10).isActive = true
+        stackView.leftAnchor.constraint(equalTo: safeArea.leftAnchor, constant: 30).isActive = true
+        stackView.rightAnchor.constraint(equalTo: safeArea.rightAnchor, constant: -30).isActive = true
+        
+        inputViews.forEach {
+            stackView.addArrangedSubview($0)
+//            $0.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        }
+        
+        stackView.bottomAnchor.constraint(equalTo: userName.bottomAnchor).isActive = true
+        
+//        self.view.addSubview(userId)
+//        userId.topAnchor.constraint(equalTo: signUpTitle.bottomAnchor, constant: 30).isActive = true
+//        userId.leftAnchor.constraint(equalTo: safeArea.leftAnchor, constant: 30).isActive = true
+//        userId.rightAnchor.constraint(equalTo: safeArea.rightAnchor, constant: -30).isActive = true
+//        userId.heightAnchor.constraint(equalToConstant: 100).isActive = true
     }
 }
 
--- Signup/Signup/Sources/Present/SignUp/View/InputView.swift
+++ Signup/Signup/Sources/Present/SignUp/View/InputView.swift
@@ -6,3 +6,63 @@
 //
 
 import Foundation
+import UIKit
+
+class InputView: UIView {
+    
+    let title: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 15, weight: .bold)
+        return label
+    }()
+    
+    let textField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.font = .systemFont(ofSize: 15)
+        textField.backgroundColor = .white
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.systemGray2.cgColor
+        return textField
+    }()
+    
+    let errorLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .systemRed
+        label.font = .systemFont(ofSize: 13)
+        label.isHidden = true
+        return label
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        layout()
+    }
+    
+    private func layout() {
+        self.addSubview(title)
+        title.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        title.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        title.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+
+        self.addSubview(textField)
+        textField.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 5).isActive = true
+        textField.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        textField.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        textField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+
+        self.addSubview(errorLabel)
+        errorLabel.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 5).isActive = true
+        errorLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        errorLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        
+        self.bottomAnchor.constraint(equalTo: errorLabel.bottomAnchor).isActive = true
+    }
+}
