c64344b3c6a145efead6671d7379f4f6317bc1cf
sol <hskim6521@naver.com>
2022-03-29 18:13:21 +0900

    Add: SignUp 프로젝트 생성

---
+++ SignUp/SignUp/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SignUp
+//
+//  Created by 김한솔 on 2022/03/29.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
+
---
+++ SignUp/SignUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SignUp
+//
+//  Created by 김한솔 on 2022/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
