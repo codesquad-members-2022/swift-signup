c92b8ac1bea2dd48680b73d2149f5afa6d2f596d
shingha <shingha1124@gmail.com>
2022-03-29 14:57:46 +0900

    사용하지 않는 코드 정리

--- Signup/Signup/Sources/Extension/TextField+Extension.swift
+++ Signup/Signup/Sources/Extension/TextField+Extension.swift
@@ -19,8 +19,8 @@ extension UITextField {
             .eraseToAnyPublisher()
     }
     
-    func addLeftPadding() {
-        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.height))
+    func addLeftPadding(_ padding: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: padding, height: self.frame.height))
         self.leftView = paddingView
         self.leftViewMode = .always
     }
