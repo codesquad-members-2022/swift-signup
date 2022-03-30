8adedd081d8d00bc17450d1b926ec5d53ad3e13d
백상휘 <sanghwiback@gmail.com>
2022-03-30 12:14:17 +0900

    Remove CustomSegueButton methods related subviews

--- SignUp/SignUp/View/CustomViews/CustomSegueButton.swift
+++ SignUp/SignUp/View/CustomViews/CustomSegueButton.swift
@@ -20,63 +20,45 @@ final class CustomSegueButton: UIButton {
     convenience init(with frame: CGRect, as title: String, using selector: Selector, direction: ButtonArrow = .right) {
         self.init(frame: frame)
         buttonTitle = title
-        
+        arrowImageDirection = direction
         addTarget(superview, action: selector, for: .touchUpInside)
-        
-        (self.subviews.first(where: {v in v is UILabel}) as? UILabel)?.text = buttonTitle
+        resetButtonViews()
     }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        let leftPadding = frame.width/6
-        let imageView = setArrowImageView(direction: .right, as: leftPadding)
-        setNameLabel(maskingView: imageView, as: leftPadding, using: buttonTitle)
+        resetButtonViews()
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.borderWidth = 1
+        self.titleLabel?.textAlignment = .center
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        let leftPadding = frame.width/6
-        let imageView = setArrowImageView(direction: .right, as: leftPadding)
-        setNameLabel(maskingView: imageView, as: leftPadding, using: buttonTitle)
-    }
-    
-    // MARK: - Add Subviews
-    
-    @discardableResult
-    private func setArrowImageView(direction: ButtonArrow, as padding: CGFloat) -> UIImageView {
-        let imageView = direction.convertUIImageView()
-        addSubview(imageView)
-        imageView.center.y = self.center.y
-        imageView.frame.origin.x = padding
-        return imageView
-    }
-    
-    @discardableResult
-    private func setNameLabel(maskingView: UIView, as padding: CGFloat, using title: String) -> UILabel {
-        let label = UILabel()
-        addSubview(label)
-        label.text = title
-        label.center.y = self.center.y
-        label.frame = CGRect(
-            x: maskingView.frame.maxX + padding/2,
-            y: self.center.y - maskingView.frame.height/2,
-            width: self.frame.width - maskingView.frame.maxX + padding/2,
-            height: maskingView.frame.height
-        )
-        label.textColor = .tintColor
-        return label
+        resetButtonViews()
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.borderWidth = 1
+        self.titleLabel?.textAlignment = .center
     }
     
     // MARK: - Utility in CustomSegueButton
     
     func changeArrowDirection() {
         arrowImageDirection = arrowImageDirection == .left ? .right : .left
-        let image = arrowImageDirection.convertUIImageView().image
-        self.subviews.forEach { subview in
-            if let imageView = subview as? UIImageView {
-                imageView.image = image
-            }
-        }
+        setImage(arrowImageDirection.convertUIImageView(), for: .normal)
+    }
+    
+    func changeTitle(in title: String) {
+        buttonTitle = title
+        setTitle(title, for: .normal)
+    }
+    
+    private func resetButtonViews() {
+        setImage(arrowImageDirection.convertUIImageView(), for: .normal)
+        setTitle(buttonTitle, for: .normal)
+        titleLabel?.frame.size.width = self.frame.width - (imageView?.frame.maxX ?? 0)
+        setTitleColor(.tintColor, for: .normal)
+        backgroundColor = .systemGray5
     }
     
     // MARK: - Arrow Image Type
@@ -85,8 +67,8 @@ final class CustomSegueButton: UIButton {
         case left = "arrow.left.circle"
         case right = "arrow.right.circle"
         
-        func convertUIImageView() -> UIImageView {
-            UIImageView(image: UIImage(systemName: self.rawValue))
+        func convertUIImageView() -> UIImage? {
+            UIImage(systemName: self.rawValue)
         }
     }
 }
