620544e188a3334b63684b40e6ebb0b4a4fc64e7
sol <hskim6521@naver.com>
2022-03-30 17:55:45 +0900

    Refactor: CustomButton과 InputComponentsView의 추상 타입 프로토콜을 생성
    CustomButton과 InputComponentsView 객체를 사용할 때 해당 객체에 대한 의존성을 낮추기 위해 추상 타입 프로토콜을 생성
    ViewFactory 구조체를 선언하여 InputComponentsView와 CustomButton의 추상 타입 객체를 생성하도록 설정
    RegisterViewController에 필요한 뷰들은 추후에 재사용될 것이므로 ViewFactory를 이용해 화면을 구성하는 View들을 만들도록 수정

--- SignUp/SignUp/View/CustomButton.swift
+++ SignUp/SignUp/View/CustomButton.swift
@@ -7,23 +7,43 @@
 
 import UIKit
 
-class CustomButton: UIButton {
+protocol CustomButtonable: UIButton {
+    func setLeftImage(systemName: String)
+    func setRightTitle(_ title: String)
+}
+
+class CustomButton: UIButton, CustomButtonable {
     
-    init(frame: CGRect, systemImageName: String = "arrowtriangle.right.circle.fill", title: String) {
+    static let fixedColor = UIColor(red: 0.35030133279999998, green: 0.81205902640000005, blue: 0.91972458960000003, alpha: 1)
+    
+    override init(frame: CGRect) {
         super.init(frame: frame)
-        let fixedColor = UIColor(red: 0.35030133279999998, green: 0.81205902640000005, blue: 0.91972458960000003, alpha: 1)
         
-        let buttonImageConfiguration = UIImage.SymbolConfiguration.init(hierarchicalColor: fixedColor)
-        self.setImage(UIImage(systemName: systemImageName, withConfiguration: buttonImageConfiguration), for: .normal)
         self.backgroundColor = .white
         self.semanticContentAttribute = .forceLeftToRight
-        self.setTitle(title, for: .normal)
-        self.setTitleColor(fixedColor, for: .normal)
+        self.setTitleColor(CustomButton.fixedColor, for: .normal)
+        self.setTitleColor(.systemGray6, for: .disabled)
+        self.layer.borderWidth = 1
+        self.layer.borderColor = CustomButton.fixedColor.cgColor
+        self.layer.cornerRadius = 5
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         self.imageView?.image = UIImage()
         self.setTitle(" ", for: .normal)
+    }
+    
+    func setLeftImage(systemName: String) {
+        guard UIImage(systemName: systemName) != nil else {return}
+        
+        let buttonEnabledConfiguration = UIImage.SymbolConfiguration.init(hierarchicalColor: CustomButton.fixedColor)
+        let buttonDisabledConfiguration = UIImage.SymbolConfiguration.init(hierarchicalColor: .systemGray6)
+        self.setImage(UIImage(systemName: systemName, withConfiguration: buttonEnabledConfiguration), for: .normal)
+        self.setImage(UIImage(systemName: systemName, withConfiguration: buttonDisabledConfiguration), for: .disabled)
+    }
+    
+    func setRightTitle(_ title: String) {
+        self.setTitle(title, for: .normal)
     }
 }
