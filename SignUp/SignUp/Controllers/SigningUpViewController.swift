d06a02287d2295e547e3c141e414ddb5381bee64
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-30 16:06:36 +0900

    Add: 회원가입 관련 View, ViewController 클래스 추가

---
+++ SignUp/SignUp/Controllers/SigningUpViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class SigningUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func unwindToSigningUpView(_ segue: UIStoryboardSegue){
+        
+    }
+
+}
+
---
+++ SignUp/SignUp/Views/SigningUpView.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class SigningUpView: UIView {
+
+    @IBOutlet weak var idTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    @IBOutlet weak var passwordCheckTextField: UITextField!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+}
