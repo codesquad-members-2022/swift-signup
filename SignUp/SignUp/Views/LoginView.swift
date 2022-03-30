33370e457f9ee181d55c120a9c63ac4e582e2500
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-30 16:06:19 +0900

    Add: LoginView, LoginViewController 클래스 추가

---
+++ SignUp/SignUp/Views/LoginView.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class LoginView: UIView {
+    
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    @IBAction func loginButtonTouched(_ sender: UIButton) {
+        
+    }
+}
