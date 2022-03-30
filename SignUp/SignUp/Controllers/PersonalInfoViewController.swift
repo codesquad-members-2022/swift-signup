f0345bfdd0ff3477a011375ea15caca847b5abd3
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-30 16:06:36 +0900

    Add: 회원가입 관련 View, ViewController 클래스 추가

--- SignUp/SignUp/Controllers/PersonalInfoViewController.swift
+++ SignUp/SignUp/Controllers/PersonalInfoViewController.swift
@@ -1,11 +1,9 @@
 import UIKit
 
-class ViewController: UIViewController {
-
+class PersonalInfoViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
 }
-
