deae347ece01ff8d20996bff521c1259e71730ea
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-30 22:09:56 +0900

    Feat: LoginView에서 사용자가 입력한 아이디와 비밀번호를 delegate object로 전달하는 기능 추가

--- SignUp/SignUp/Views/LoginView.swift
+++ SignUp/SignUp/Views/LoginView.swift
@@ -5,9 +5,12 @@ class LoginView: UIView {
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
+    var delegate: LoginViewDelegate?
     
     @IBAction func loginButtonTouched(_ sender: UIButton) {
-        
+        guard let idInput = self.idTextField.text else { return }
+        guard let passwordInput = self.passwordTextField.text else { return }
+        self.delegate?.sendIdAndPasswordInput(id: idInput, password: passwordInput)
     }
 
     @IBAction func idInputEditingChanged(_ sender: UITextField) {
@@ -16,6 +19,10 @@ class LoginView: UIView {
     
     @IBAction func passwordInputEditingChanged(_ sender: Any) {
         self.checkLoginButtonValidation()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.endEditing(true)
     }
     
     private func checkLoginButtonValidation(){
