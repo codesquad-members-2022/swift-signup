7cf6752bb24821db3d9b2aa1a7f6f5ba3c43dcd5
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-30 22:10:36 +0900

    Feat: AlertPresentable 프로토콜 추가 후 LoginViewController에서 채택시켜서 http 응답에 따라 알림창 띄우도록 수정

--- SignUp/SignUp/Controllers/LoginViewController.swift
+++ SignUp/SignUp/Controllers/LoginViewController.swift
@@ -24,8 +24,26 @@ extension LoginViewController: LoginViewDelegate{
     @objc func responseReceived(_ notification: Notification){
         guard let responseBody = notification.userInfo?[HttpResponseKey.responseBodyData] as? Dictionary<String,String> else { return }
         guard let status = responseBody["status"] else { return }
+        
+        var alertMessage: String = ""
+        if(status == "409"){
+            alertMessage = "로그인 성공"
+        }else{
+            alertMessage = "로그인 실패"
+        }
+        self.presentAlert(title: "로그인 테스트", message: alertMessage)
     }
     
 }
+
+extension LoginViewController: AlertPresentable{
+    
+    func presentAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let defaultAction =  UIAlertAction(title: "확인", style: UIAlertAction.Style.default)
+        alert.addAction(defaultAction)
+        DispatchQueue.main.async {
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 }
