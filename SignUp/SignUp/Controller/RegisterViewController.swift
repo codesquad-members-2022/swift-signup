98fc3d6c3106899053cbb95027c5ecdf140020bf
sol <hskim6521@naver.com>
2022-03-30 11:37:43 +0900

    Add: 회원가입 화면에 해당하는 ViewController 추가
    Controller 폴더로 WelcomeViewController, RegisterViewController 이동

---
+++ SignUp/SignUp/Controller/RegisterViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterViewController.swift
+//  SignUp
+//
+//  Created by 김한솔 on 2022/03/29.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    static let identifier = "RegisterViewController"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
