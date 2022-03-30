a1b0f294a4e9c5a6e57c002b1447159c2bb164ca
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-30 22:09:19 +0900

    Feat: LoginViewController에 LoginViewDelegate 프로토콜 채택 후, 뷰에서 입력받은 아이디와 비밀번호 값 전달받는 기능 추가

--- SignUp/SignUp/Controllers/LoginViewController.swift
+++ SignUp/SignUp/Controllers/LoginViewController.swift
@@ -1,10 +1,31 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var loginView: LoginView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginView.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(responseReceived(_:)), name: NotificationName.normalResponseReceived, object: HttpRequestHandler.self)
+    }
+}
+
+extension LoginViewController: LoginViewDelegate{
+    
+    func sendIdAndPasswordInput(id: String, password: String) {
+        let loginUser = LoginUser(id: id, password: password)
+        let jsonData = try? JSONEncoder().encode(loginUser)
+        if let url = URL(string: "https://api.codesquad.kr/signup"){
+            HttpRequestHandler.sendRequest(data: jsonData, url: url, httpMethod: HttpMethod.post){}
+        }
+    }
+    
+    @objc func responseReceived(_ notification: Notification){
+        guard let responseBody = notification.userInfo?[HttpResponseKey.responseBodyData] as? Dictionary<String,String> else { return }
+        guard let status = responseBody["status"] else { return }
+    }
+    
+}
     }
 }
