481e1d832344c10358119c82c7a8a71334fcb3b5
백상휘 <sanghwiback@gmail.com>
2022-03-30 11:43:05 +0900

    Add class representing CustomTextField

---
+++ SignUp/SignUp/Model/CustomTextFieldDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  CustomTextFieldDelegate.swift
+//  SignUp
+//
+//  Created by 백상휘 on 2022/03/29.
+//
+
+import Foundation
+import UIKit
+
+class CustomTextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+    var commentLabelIn: (UITextField) -> CustomCommentLabel? = { textField in
+        (textField as? CustomTextField)?.commentLabel
+    }
+    
+    var validatorIn: (UITextField) -> ValidationRegularText? = { textField in
+        (textField as? CustomTextField)?.validator
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        commentLabelIn(textField)?.hideComment()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let targetString = textField.text else { return }
+        
+        guard targetString.count == 0 else {
+            commentLabelIn(textField)?.hideComment()
+            return
+        }
+        
+        let _ = validatorIn(textField)?.matches(in: targetString, range: NSMakeRange(0, targetString.count))
+        commentLabelIn(textField)?.showComment(validatorIn(textField)?.comment)
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let targetString = textField.text else { return false }
+        let _ = validatorIn(textField)?.matches(in: targetString, range: NSMakeRange(0, targetString.count))
+        commentLabelIn(textField)?.showComment(validatorIn(textField)?.comment)
+        
+        return true
+    }
+}
--- SignUp/SignUp/View/CustomViews/CustomTextField.swift
+++ SignUp/SignUp/View/CustomViews/CustomTextField.swift
@@ -11,11 +11,10 @@ final class CustomTextField: UITextField, UITextFieldDelegate {
     
     // MARK: - Local Properties
     
-    private lazy var commentLabel = CustomCommentLabel(frame: self.frame)
+    private(set) lazy var commentLabel = CustomCommentLabel(frame: self.frame)
     var validator: ValidationRegularText?
     
     // MARK: - Initialzers
-
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.delegate = self
@@ -26,31 +25,5 @@ final class CustomTextField: UITextField, UITextFieldDelegate {
         super.init(coder: coder)
         self.delegate = self
         addSubview(commentLabel)
-    }
-
-    // MARK: - Methods in delegate
-    
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        commentLabel.hideComment()
-        return true
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        guard let targetString = textField.text else { return }
-        
-        guard targetString.count == 0 else {
-            commentLabel.hideComment()
-            return
-        }
-        let _ = validator?.matches(in: targetString, range: NSMakeRange(0, targetString.count))
-        commentLabel.showComment(validator?.comment)
-    }
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        guard let targetString = textField.text else { return false }
-        let _ = validator?.matches(in: targetString, range: NSMakeRange(0, targetString.count))
-        commentLabel.showComment(validator?.comment)
-        
-        return true
     }
 }
