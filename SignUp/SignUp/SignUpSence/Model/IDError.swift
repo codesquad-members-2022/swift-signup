058e5c3c3055c9c63657d8d73411a46b7e0afe7d
P_SeoB <akdlsjtm21@naver.com>
2022-03-31 15:31:18 +0900

    Feat:Add TextField Error type

---
+++ SignUp/SignUp/SignUpSence/Model/IDError.swift
@@ -0,0 +1,11 @@
+//
+//  IDError.swift
+//  SignUp
+//
+//  Created by 박진섭 on 2022/03/31.
+//
+
+enum inValidateIDCase {
+    case usedID
+    case elseError
+}
---
+++ SignUp/SignUp/SignUpSence/Model/PasswordError.swift
@@ -0,0 +1,13 @@
+//
+//  PasswordError.swift
+//  SignUp
+//
+//  Created by 박진섭 on 2022/03/31.
+//
+
+enum inValidatePasswordCase {
+    case noValidateLength
+    case noUpperCharacter
+    case noNumber
+    case noSpecialCharacter
+}
