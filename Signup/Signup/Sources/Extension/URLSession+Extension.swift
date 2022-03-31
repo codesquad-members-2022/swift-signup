6d76a6c79ad990c1e65ace55211832c3fc2ed36c
shingha <shingha1124@gmail.com>
2022-03-31 15:14:11 +0900

    [review] 사용하지 않는 코드 제거

--- Signup/Signup/Sources/Extension/URLSession+Extension.swift
+++ Signup/Signup/Sources/Extension/URLSession+Extension.swift
@@ -30,30 +30,6 @@ extension URLSession {
             .eraseToAnyPublisher()
     }
     
-    func jsonDecoder<T: Decodable>(_ type: T.Type, for url: URL) -> AnyPublisher<T, Error> {
-        self.dataTaskPublisher(for: url)
-            .tryMap { data, response -> Data in
-                if let error = self.checkResponse(response) {
-                    throw error
-                }
-                return data
-            }
-            .decode(type: type.self, decoder: JSONDecoder())
-            .eraseToAnyPublisher()
-    }
-    
-    func downloadImages(urls: [URL], completion: @escaping (URL?) -> Void) {
-        urls.forEach {
-            downloadImage(url: $0, completion: completion)
-        }
-    }
-    
-    func downloadImage(url: URL, completion: @escaping (URL?) -> Void) {
-        URLSession.shared.downloadTask(with: url) { url, response, error in
-            completion(url)
-        }.resume()
-    }
-    
     private func checkResponse(_ response: URLResponse?) -> ResponseError? {
         if let response = response as? HTTPURLResponse,
            (200..<300).contains(response.statusCode) == false {
