3cba5cdf270e771ff7ebbbf4da2d15bfeb8f3274
백상휘 <sanghwiback@gmail.com>
2022-04-01 16:26:59 +0900

    CleanUp before Pull Request
    PR 전에 파일 이름을 정리하고
    필요없는 주석, 파일 등을 제거하였습니다.

--- SignUp/SignUp/Model/Validation/ValidationRegularText.swift
+++ SignUp/SignUp/Model/Validation/ValidationRegularText.swift
@@ -27,11 +27,6 @@ class ValidationRegularText: ValidationCommons {
     
     // MARK: - matching methods
     
-//    func validateResult(in string: String) {
-//        guard string.count > 0 else { return }
-//        let results = regularExpression.matches(in: string, range: NSMakeRange(0, string.count))
-//        validationResult.validateResultState(in: string, using: results)
-//    }
     override func validate(using string: String) {
         super.validate(using: string)
         let results = regularExpression.matches(in: string, range: NSMakeRange(0, string.count))
