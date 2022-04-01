1d026c436c4cc338ce5fd6ae2eff8af8fbbc76d9
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-04-02 00:33:08 +0900

    Refactor: HttpRequestHandler에서 응답 오류 발생 시 로그 처리하던 부분을 모두 제거하고 클로저 호출로 대체
    - 로그 찍는 코드 모두 제거
    - 오류가 발생했다고 판단되면, Result<Data,Error> 타입을 파라미터로 받는 클로저를 호출하도록 변경

--- SignUp/SignUp/Common/HttpRequestHandler.swift
+++ SignUp/SignUp/Common/HttpRequestHandler.swift
@@ -1,5 +1,4 @@
 import Foundation
-import OSLog
 
 enum HttpMethod: String{
     case get = "GET"
@@ -9,29 +8,43 @@ enum HttpMethod: String{
 
 }
 
+enum HttpError: Error, CustomStringConvertible{
+    case normalError(error: Error)
+    case dataNotReceivedError
+    case requestError
+    
+    var description: String{
+        switch self {
+        case .normalError(let error):
+            return error.localizedDescription
+        case .dataNotReceivedError:
+            return "data not received"
+        case .requestError:
+            return "client request error"
+        }
+    }
 }
 
 class HttpRequestHandler{
     
-    static let logger = Logger()
     static func sendRequest(data: Data?, url: URL, httpMethod: HttpMethod, completion: @escaping ()->Void){
         var request = URLRequest(url: url)
         request.httpMethod = httpMethod.rawValue
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpBody = data
         URLSession.shared.dataTask(with: request){ data, response, error in
-            guard error == nil else {
-                self.logger.error("\(error.debugDescription)")
+            if let error = error{
+                completion(.failure(HttpError.normalError(error: error)))
                 return
             }
-            
+
             guard let data = data, let response = response as? HTTPURLResponse else {
-                self.logger.error("Could not receive any response or data")
+                completion(.failure(HttpError.dataNotReceivedError))
                 return
             }
-            
+
             if(response.statusCode >= 400){
-                self.logger.debug("detected response with \(response.statusCode) status\nurl : \(url)")
+                completion(.failure(HttpError.requestError))
                 return
             }
             
